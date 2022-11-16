b58b21e2a9ce6a064ea30611cd5a859b4702deb8
Kevin Dutzy <kddutz15@gmail.com>
2022-11-16 12:44:54 -0500

    changes to styling and misc updates

--- tor.js
+++ tor.js
@@ -1,4 +1,4 @@
-const token = "BQB51FFPNmbcZd9KFg12fXqIqnQeQf-fFIg9WsKDw8EbBt4IBbM1GLnODYJRFlsccHOA0nv58vYgky6PVS9Wmog2Q_9eKEwvFomURrDMvUBqoA4z_veqAo9XATuVxp6KUXNkky1tFSbyM--DUHG3rCQVs-FSec40Ek-t0g_GVSr4"
+const token = "BQDcSVbQRzO7e6FIruuZXh0as7kbXyKr9_6v5b-dQJBnKWjfXtpoRR_bhxbp4Ryf0HMQSnQWZeBpUsN97vjNVdZ1f-ZISdOL8YqihcknpaHHMdBM0nc7TpQ3b94Kn0eMrhbe5a23zc9lBf-rpuk9o_p9W5B7JgVbf45dHJb1qLQj6nw"
 
 const searchForm = document.querySelector('#search-bar')
 let userSearchInput = document.querySelector('#submit')
